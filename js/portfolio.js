7f86c3ce83fcde6158d01a0932bf732a2d53e4e1
brandongossen <brandongossen@yahoo.com>
2017-09-24 21:00:08 -0500

    Fixed the empty whitespace at the bottom of the page
    -Add images that display when a main tab is clicked.
    
    -Replace the middle picture with a thinking robot.
    
    -Add functionality that makes the robot disappear when one of the main tabs is clicked to make room for the image that will appear.
    
    -Create margins to keep everything aligned via css.
    
    -Add a link to my resume page.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,138 @@
-alert("Tacos for Tim Duncan");
+(function () {
+
+
+    // $(document).ready(function(){
+        // $("a.konami").tooltip();
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $("#aboutMe").click(function () {
+        $(".img-circle").slideToggle();
+    });
+
+    $("#aboutMe").click(function () {
+        $(".img-robot").slideToggle();
+    });
+
+    $("#portfolio").click(function () {
+        $(".img-briefcase").slideToggle();
+    });
+
+    $("#portfolio").click(function () {
+        $(".img-robot").slideToggle();
+    });
+
+    $("#resume").click(function () {
+        $(".img-resume").slideToggle();
+    });
+
+    $("#resume").click(function () {
+        $(".img-robot").slideToggle();
+    });
+    //     function () {
+    //         $(".img-robot").slideToggle();
+    // });
+//     (function () {
+//         "use strict";
+//         $(document).ready(function () {
+//             $("#highlight").click(function (event) {
+//                 event.preventDefault();
+//                 $("dd").toggleClass("invisible");
+//                 //have to put "dd" to select and unselect because the invisible class would disappear from the dd element after clicking the anchor, but the element would not disappear.
+//             });
+//         });
+//
+//         $("dt").click(function () {
+// //			    $("dt").css("background-color", "#FFF");
+//             $(this).css("background-color", "#7CB342");
+//         });
+//
+//         //Transversing Section
+//
+// //			$("#yellow-bkgr").click(function(event){
+// //                $(".four-facts-about-the-park").children().next().next().next().css("background-color", "yellow");
+// //            });
+//
+//
+//         $("#yellow-bkgr").click(function (event) {
+//             $("ul").each(function (index) {
+//                 $(this).children().last().css("background-color", "yellow");
+//             });
+//         });
+// //			This version is the better way than the .next().next.next() version that why I commented out that version and kept this
+//
+//
+// //            var list = $("h3").next().children();
+// //			console.log(list);
+//         $("h3").click(function (event) {
+//             $(this).next().children().css("font-weight", "bold");
+//         });
+//
+//         $("li").click(function (event) {
+//             $(this).parent().children().first().css("color", "blue");
+//         });
+//
+//         $(".swap").click(function (event) {
+//
+//
+//             if ($(this).is(".swap:last")) {
+//                 var imgThree = $(this).prev(); // third image
+//                 var imgOneFirst = $(this).parent().prev().prev().children().first(); // first image
+//                 var imgOneSrc = imgOneFirst.attr("src");
+//
+//                 imgOneFirst.attr("src", imgThree.attr("src"));
+//                 imgThree.attr("src", imgOneSrc);
+//                 // put the image on this div in the first div
+//                 // put the image in the first div here
+//             } else {
+//                 var imgOne = $(this).prev(); // current image
+//                 var imgTwo = $(this).parent().next().children().first();  //image at the right
+//
+//
+//                 var imgTwoSrc = imgTwo.attr("src"); // the src/url of the image at the right
+//
+//                 imgTwo.attr("src", imgOne.attr("src"));
+//                 imgOne.attr("src", imgTwoSrc);
+//
+//             }
+//
+//         }); //(image)
+//
+//
+// //			JQuery Effects Exercise
+//         $("span").click(function () {
+//             $(this).parent().hide();
+//         });
+//
+//         $("h3").click(function () {
+//             $(this).next().slideToggle();
+//         });
+//
+//
+//         function fadeRegister() {
+//             console.log("it runs");
+//             $("#register").fadeIn();
+//         }
+//
+//         setTimeout(fadeRegister, 8000);
+//
+//         function modalAppear() {
+//             $("#myModal").modal();
+//         }
+//
+//         setTimeout(modalAppear, 8000);
+//
+//
+//
+//     // $("#keyLock").hover(
+//     //     function () {
+//     //         $("#konami").show();
+//     //     },
+//     //     function () {
+//     //         $("#konami").hide();
+//     //     }
+//     // );
+//
+//
+//     // });
+})();
