df3d32164f33818b0d86f4a465bb7ac8d5623cee
brandongossen <brandongossen@yahoo.com>
2017-12-16 00:51:57 -0600

    Notify users to click briefcase
    -Add directions below the briefcase icon that tell users to click the icon.
    
    -Make it mobile responsive.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,4 @@
-(function () {
+$(document).ready(function () {
 
 
     // $(document).ready(function(){
@@ -36,6 +36,7 @@
 
         if($(".img-briefcase").css('display') == 'inline') {
             $(".img-briefcase").slideUp();
+            $(".briefcase-click").slideUp();
             briefcaseCount++;
             briefcaseOpen = false;
         }
@@ -54,10 +55,12 @@
         e.preventDefault();
         if(briefcaseCount % 2 == 0){
             $(".img-briefcase").slideDown();
+            $(".briefcase-click").slideDown();
             $(".img-robot").slideUp();
             briefcaseOpen = true;
         }else{
             $(".img-briefcase").slideUp();
+            $(".briefcase-click").slideUp();
             $(".img-robot").slideToggle();
             briefcaseOpen = false;
         }
@@ -96,6 +99,7 @@
         resumeCount++;
         if($(".img-briefcase").css('display') == 'inline') {
             $(".img-briefcase").slideUp();
+            $(".briefcase-click").slideUp();
             briefcaseOpen = false;
             briefcaseCount++;
         }
@@ -107,115 +111,5 @@
             circleCount++;
             circleOpen = false;
         }
-
     });
-
-    // $("#resume").click(function () {
-    // });
-    //     function () {
-    //         $(".img-robot").slideToggle();
-    // });
-//     (function () {
-//         "use strict";
-//         $(document).ready(function () {
-//             $("#highlight").click(function (event) {
-//                 event.preventDefault();
-//                 $("dd").toggleClass("invisible");
-//                 //have to put "dd" to select and unselect because the invisible class would disappear from the dd element after clicking the anchor, but the element would not disappear.
-//             });
-//         });
-//
-//         $("dt").click(function () {
-// //			    $("dt").css("background-color", "#FFF");
-//             $(this).css("background-color", "#7CB342");
-//         });
-//
-//         //Transversing Section
-//
-// //			$("#yellow-bkgr").click(function(event){
-// //                $(".four-facts-about-the-park").children().next().next().next().css("background-color", "yellow");
-// //            });
-//
-//
-//         $("#yellow-bkgr").click(function (event) {
-//             $("ul").each(function (index) {
-//                 $(this).children().last().css("background-color", "yellow");
-//             });
-//         });
-// //			This version is the better way than the .next().next.next() version that why I commented out that version and kept this
-//
-//
-// //            var list = $("h3").next().children();
-// //			console.log(list);
-//         $("h3").click(function (event) {
-//             $(this).next().children().css("font-weight", "bold");
-//         });
-//
-//         $("li").click(function (event) {
-//             $(this).parent().children().first().css("color", "blue");
-//         });
-//
-//         $(".swap").click(function (event) {
-//
-//
-//             if ($(this).is(".swap:last")) {
-//                 var imgThree = $(this).prev(); // third image
-//                 var imgOneFirst = $(this).parent().prev().prev().children().first(); // first image
-//                 var imgOneSrc = imgOneFirst.attr("src");
-//
-//                 imgOneFirst.attr("src", imgThree.attr("src"));
-//                 imgThree.attr("src", imgOneSrc);
-//                 // put the image on this div in the first div
-//                 // put the image in the first div here
-//             } else {
-//                 var imgOne = $(this).prev(); // current image
-//                 var imgTwo = $(this).parent().next().children().first();  //image at the right
-//
-//
-//                 var imgTwoSrc = imgTwo.attr("src"); // the src/url of the image at the right
-//
-//                 imgTwo.attr("src", imgOne.attr("src"));
-//                 imgOne.attr("src", imgTwoSrc);
-//
-//             }
-//
-//         }); //(image)
-//
-//
-// //			JQuery Effects Exercise
-//         $("span").click(function () {
-//             $(this).parent().hide();
-//         });
-//
-//         $("h3").click(function () {
-//             $(this).next().slideToggle();
-//         });
-//
-//
-//         function fadeRegister() {
-//             console.log("it runs");
-//             $("#register").fadeIn();
-//         }
-//
-//         setTimeout(fadeRegister, 8000);
-//
-//         function modalAppear() {
-//             $("#myModal").modal();
-//         }
-//
-//         setTimeout(modalAppear, 8000);
-//
-//
-//
-//     // $("#keyLock").hover(
-//     //     function () {
-//     //         $("#konami").show();
-//     //     },
-//     //     function () {
-//     //         $("#konami").hide();
-//     //     }
-//     // );
-//
-//
-//     // });
 })();
